8bb7811c7ad6370fb173c565e04f8b7278124c65
ashgz8025 <ashelle.gomes@gmail.com>
2024-12-01 14:38:16 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background-color: #1d1f27;
+  color: #ffffff;
+}
+</style>
